2895af621f24e58a98f0000409065dc04ce24ad7
leolego03 <leolego03@gmail.com>
2022-07-13 16:18:16 +0900

    Added business logic of Article

--- src/main/java/com/leolego03/board/article/ArticleService.java
+++ src/main/java/com/leolego03/board/article/ArticleService.java
@@ -1,10 +1,12 @@
 package com.leolego03.board.article;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ArticleService {
@@ -21,17 +23,45 @@ public class ArticleService {
 	}
 
 	public void addNewArticle(Article article) {
-		// TODO Auto-generated method stub
-		
+		articleRepository.save(article);
 	}
 
 	public void deleteArticle(Long articleId) {
-		// TODO Auto-generated method stub
-		
+		boolean exists = articleRepository.existsById(articleId);
+		if (!exists) {
+				throw new IllegalStateException(
+								"article with id " + articleId + " does not exists");
+		}
+		articleRepository.deleteById(articleId);
 	}
 
-	public void updateArticle(Long articleId, String name, String subject, String content, LocalDate date) {
-		// TODO Auto-generated method stub
+	@Transactional
+	public void updateArticle(Long articleId,
+								String name,
+								String subject,
+								String content,
+								LocalDate date) {
+		Article article = articleRepository.findById(articleId)
+							.orElseThrow(() -> new IllegalStateException(
+											"article with id " + articleId + " does not exists"));
+		
+		if (name != null &&
+						name.length() > 0 &&
+						!Objects.equals(article.getName(), name)) {
+				article.setName(name);
+		}
+		
+		if (subject != null &&
+				subject.length() > 0 &&
+				!Objects.equals(article.getSubject(), subject)) {
+		article.setSubject(subject);
+		}
+		
+		if (content != null &&
+				content.length() > 0 &&
+				!Objects.equals(article.getContent(), content)) {
+		article.setContent(content);
+		}
 		
 	}
 }
